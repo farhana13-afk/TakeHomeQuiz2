3df5a0af15ec32a9f988aa404bc94e9bbffa5927
harshita-sudhakar <92887925+harshita-sudhakar@users.noreply.github.com>
2024-11-26 14:10:24 -0500

    Commit Lamba Expression
    Added Lambda Expression to Main

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main (String [] args) {
+import java.util.*;
 
+public class Main
+{
+    public static void main(String [] args)
+    {
+        /*Scanner reader = new Scanner(System.in);
+        System.out.println("Type in Two Numbers");
+        System.out.print("Lower Bound: ");
+        int low = reader.nextInt();
+        System.out.print("Upper Bound: ");
+        int upp = reader.nextInt();*/
+
+        Addition add = (int lower, int higher) ->
+        {
+            int sum = 0;
+            for(int i= lower; i<=higher; i++)
+            {
+                sum +=i;
+            }
+            return sum;
+        };
+
+        System.out.println("Sum of Numbers Between: " + add.addNumbers(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
     }
 }
